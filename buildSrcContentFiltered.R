78b442527b35ad464b6256711be50f425aeb844b
Elloumi <elloumif@nci-02048674-ml.nih.gov>
2019-01-30 12:23:44 -0500

    Added new functions to restrict content to specific cell lines based on oncotree1 and oncotree2

---
+++ buildSrcContentFiltered.R
@@ -0,0 +1,21 @@
+library(rcellminer)
+
+config <- jsonlite::fromJSON("config_filtered.json") # could be original config file
+
+source("appUtils.R")
+source("dataLoadingFunctions.R")
+# here an example for SCLC
+srcContent <- lapply(config, loadSourceContentFiltered,onco1="Lung",onco2="Small Cell Lung Cancer (SCLC)")
+
+isLoadedSrc <- vapply(srcContent, function(x) { !is.null(x) }, logical(1))
+if (any(!isLoadedSrc)){
+	srcContent <- srcContent[isLoadedSrc]
+}
+
+# For NCI-60, replace default color map to use CellMiner tissue type colors.
+# nci60ColorTab <- loadNciColorSet(returnDf=TRUE)
+# nci60ColorTab$OncoTree1 <- srcContent$nci60$sampleData$OncoTree1
+# srcContent$nci60$tissueColorMap <- c(by(nci60ColorTab, nci60ColorTab$OncoTree1, 
+# 																				FUN = function(x) unique(x$colors)))
+
+saveRDS(srcContent, "srcContent_filtered).rds", compress = FALSE)
